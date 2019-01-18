28531fd9b71f7febb1e4cbef5096c1d7523dc10f
leezaweb <leeza.web@gmail.com>
2019-01-18 09:50:28 -0500

    click event opacity

--- features-map/js/features.js
+++ features-map/js/features.js
@@ -235,6 +235,38 @@ function makeMarkers(nation, json, filters) {
       });
     },
     onEachFeature: function onEachFeature(feature, layer) {
+      layer.on({
+        click: function click(e) {
+          var isSpiderfied = false;
+
+          if (!layer._popupHandlersAdded) {
+            Object.keys(map._layers).forEach(function(l, i) {
+              if (true) {
+                if (map._layers[l].unspiderfy) map._layers[l].unspiderfy();
+              }
+            });
+
+            Object.keys(nations).forEach(function(n) {
+              Object.values(
+                nation_marker_clusters[n]._featureGroup._layers
+              ).forEach(function(v) {
+                if (v._group && v._group._spiderfied) isSpiderfied = true;
+              });
+            });
+            Array.from(
+              document.querySelectorAll("div.leaflet-marker-icon")
+            ).forEach(function(d) {
+              return (d.style.opacity = isSpiderfied ? 0.2 : 1);
+            });
+            Array.from(
+              document.querySelectorAll("img.leaflet-marker-icon")
+            ).forEach(function(d) {
+              return (d.style.opacity = isSpiderfied ? 0.2 : 1);
+            });
+          }
+        }
+      });
+
       var description;
 
       if (window.innerWidth > 768) {
@@ -270,13 +302,15 @@ function makeMarkers(nation, json, filters) {
         feature.properties.name.toLowerCase().replace(/ /g, "-");
 
       var islandTracker =
-        '<div class=\n  "popupHeaderStyle">Link</div><div class="islandTracker popupEntryStyle"><a href="https://amti.csis.org/' +
-        link +
-        '">' +
-        link.replace("https://", " ") +
-        "</a>" +
-        externalLink +
-        "</div>";
+        nation !== "unoccupied"
+          ? '<div class=\n  "popupHeaderStyle">Link</div><div class="islandTracker popupEntryStyle"><a href=' +
+            link +
+            ' target="_blank">' +
+            link.replace("https://", " ") +
+            "</a>" +
+            externalLink +
+            "</div>"
+          : "";
 
       layer.bindPopup(description + islandTracker);
     }
@@ -286,15 +320,57 @@ function makeMarkers(nation, json, filters) {
 
   map.addLayer(nation_marker_clusters[nation]);
 
-  nation_marker_clusters[nation].on("clustermouseover", function(a) {
+  nation_marker_clusters[nation].on("clusterclick", function(a) {
     map._layers[a.layer._leaflet_id].spiderfy();
+
     Object.keys(map._layers).forEach(function(layer, i) {
       if (parseInt(layer, 10) !== a.layer._leaflet_id) {
         if (map._layers[layer].unspiderfy) map._layers[layer].unspiderfy();
       }
     });
+
+    var isSpiderfied = false;
+
+    Object.keys(nations).forEach(function(n) {
+      Object.values(nation_marker_clusters[n]._featureGroup._layers).forEach(
+        function(v) {
+          if (v._group && v._group._spiderfied) isSpiderfied = true;
+        }
+      );
+    });
+    Object.keys(nations).forEach(function(n) {
+      Array.from(document.querySelectorAll("div.leaflet-marker-icon")).forEach(
+        function(d) {
+          return (d.style.opacity = isSpiderfied ? 0.2 : 1);
+        }
+      );
+      Array.from(document.querySelectorAll("img.leaflet-marker-icon")).forEach(
+        function(d) {
+          return (d.style.opacity = isSpiderfied ? 0.2 : 1);
+        }
+      );
+      Object.values(nation_marker_clusters[n]._featureGroup._layers).filter(
+        function(v) {
+          a.layer
+            .getAllChildMarkers()
+            .map(function(m) {
+              return m.getElement();
+            })
+            .filter(function(m) {
+              return m;
+            })
+            .forEach(function(m) {
+              return (m.style.opacity = 1);
+            });
+        }
+      );
+    });
   });
 }
-
+function flattenDeep(arr1) {
+  return arr1.reduce(function(acc, val) {
+    return Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val);
+  }, []);
+}
 var externalLink =
   '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">><path d="M7.49,0V1.67H1.68V13.32H13.32V7.52H15v5.72a1.76,1.76,0,0,1-.42,1.19,1.64,1.64,0,0,1-1.13.56H1.74a1.67,1.67,0,0,1-1.16-.41A1.61,1.61,0,0,1,0,13.48v-.27C0,9.4,0,5.6,0,1.8A1.83,1.83,0,0,1,.58.4a1.53,1.53,0,0,1,1-.39h6Z" transform="translate(0 0)"/><path d="M9.17,1.67V0H15V5.84H13.34v-3h0c-.05.05-.11.1-.16.16l-.45.46-1.3,1.29-.84.84-.89.9-.88.87-.89.9c-.28.29-.57.57-.86.86L6.16,10l-.88.87a1.83,1.83,0,0,1-.13.16L4,9.86l0,0L5.36,8.47l.95-1,.75-.75,1-1L8.87,5l1-.94.85-.86.92-.91.56-.58Z" transform="translate(0 0)"/></svg>';
