36834af259f883b63601b28364b5dcb2f2ece821
leezaweb <leeza.web@gmail.com>
2019-05-22 11:33:06 -0400

    remove interpolation

--- claims-map/claims.js
+++ claims-map/claims.js
@@ -1,24 +1,29 @@
 var mapboxStyle =
-  lang && lang.indexOf("zh-") > -1
-    ? (mapboxStyle = "citui3waw00162jo1zcsf1urj")
-    : (mapboxStyle = "cj84s9bet10f52ro2lrna50yg");
+  lang && lang.indexOf('zh-') > -1
+    ? (mapboxStyle = 'citui3waw00162jo1zcsf1urj')
+    : (mapboxStyle = 'cj84s9bet10f52ro2lrna50yg')
 
 makeMap({
-  mapID: "claims-map",
-  googleSheet: "14MvucMac-lYCu0-2vD5tcxfCUqIJog2h4-REFkpH3Kw",
-  "mapbox style": mapboxStyle,
-  "ocean color": "#cad2d3",
+  mapID: 'claims-map',
+  googleSheet: '14MvucMac-lYCu0-2vD5tcxfCUqIJog2h4-REFkpH3Kw',
+  'mapbox style': mapboxStyle,
+  'ocean color': '#cad2d3',
 
   formatPopupContent: function(feature, map) {
-    var prefix = lang ? "_" + lang : "";
+    var prefix = lang ? '_' + lang : ''
 
-    var name = feature.properties[`name${prefix}`];
+    var name = feature.properties['name' + prefix]
 
-    var description = feature.properties[`description${prefix}`]
+    var description = feature.properties['description' + prefix]
       .replace(/<a href=/gi, '<a target="_blank" href=')
-      .replace(/<\/a>/gi, externalLink + "</a>");
+      .replace(/<\/a>/gi, externalLink + '</a>')
 
-    return `<div class="popupHeaderStyle">${name}</div>
-    <div class="popupEntryStyle">${description}</div>`;
+    return (
+      '<div class="popupHeaderStyle">' +
+      name +
+      '</div><div class="popupEntryStyle">' +
+      description +
+      '</div>'
+    )
   }
-});
+})
