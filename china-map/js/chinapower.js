3b14c89aea01f14a32a18bece0543c5001a8ff98
leezaweb <leeza.web@gmail.com>
2019-06-03 16:24:57 -0400

    restart china power map

---
+++ china-map/js/chinapower.js
@@ -0,0 +1,31 @@
+;(async function() {
+  var map = await makeMap({
+    googleSheet: '1R9J3haGLDsRPhtT1P1JvQL_XzaPZZsa33vBFO6xs6g4',
+    mapID: 'chinapower',
+    mapboxStyle:
+      lang && lang.indexOf('zh-') > -1
+        ? (mapboxStyle = 'citui3waw00162jo1zcsf1urj')
+        : (mapboxStyle = 'cj84s9bet10f52ro2lrna50yg'),
+    formatPopupContent: function(feature, map) {
+      var prefix = lang ? '_' + lang : ''
+
+      var name = feature.properties['name' + prefix]
+
+      var description = feature.properties['description' + prefix]
+        .replace(/<a href=/gi, '<a target="_blank" href=')
+        .replace(/<\/a>/gi, externalLink + '</a>')
+
+      return (
+        '<div class="popupEntryStyle">' +
+        feature.properties.reef +
+        '<br/>' +
+        name +
+        (feature.properties.observed ? '<br/>(expected)' : '') +
+        '</div>' +
+        '<div class="popupEntryStyle">' +
+        description +
+        '</div>'
+      )
+    }
+  })
+})()
