155b04cd62be7d1be10acfb93efeb2d5e0c45bb2
Serven Maraghi <maraghiserven@gmail.com>
2020-04-07 13:07:39 -0400

    Remove unused code

--- claims-map/js/claims.js
+++ claims-map/js/claims.js
@@ -9,11 +9,6 @@ makeMap({
   'mapbox style': mapboxStyle,
   'ocean color': '#cad2d3',
 
-  // makeDropdownOptions: function (options, x) {
-  //   console.log(options)
-
-  // },
-
   formatPopupContent: function (feature, map) {
     var suffix = lang ? '_' + lang.replace(/-/, '_') : ''
 
